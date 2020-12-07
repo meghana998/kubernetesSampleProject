43985ca69cf5222c26192bbf055a08e31c0e442f
matta.meghana@gmail.com <matta.meghana@gmail.com>
2020-12-07 14:52:43 +0530

    yamladded

---
+++ learnings-master/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dockersampleconfig.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getvalues")]
+        public List<string> getvalues()
+        {
+            //comment added
+            List<string> l = new List<string>();
+            l.Add("value1");
+            l.Add("value2");
+            l.Add("value3");
+            l.Add("Value4");
+            l.Add("Value5");
+            l.Add("Value6");
+            l.Add("Value7");
+            return l;
+        }
+    }
+}
